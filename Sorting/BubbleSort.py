b5edf32e28b2a2a3c66183680bf4edee35e9ace3
Ashmit <ashmitbhardwaj05@gmail.com>
2023-09-22 18:27:00 +0530

    sorting algorithms

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,10 @@
+def bubbleSort(arr) :
+    for i in range(0,len(arr)) :
+        for j in range(0,len(arr)-i-1) :
+            if arr[j] > arr[j+1] :
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+nums = [1,5,3,2,6,2,4,9]
+bubbleSort(nums)
+print(nums)
